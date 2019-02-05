fa5e7f086eb2595417f61c22fa6c7a37baf5fa75
Lucas Rytter <46856347+Yum1x@users.noreply.github.com>
2019-02-05 12:28:22 -0200

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,19 @@
+files {
+    'index.html',
+    'js/music.js',
+    'css/style.css',
+    'img/1.jpg',
+    'img/2.jpg',
+    'img/3.jpg',
+    'img/4.jpg',
+    'img/5.jpg',
+	'music/1.mp3',
+	'music/2.mp3',
+	'music/3.mp3',
+	'music/4.mp3',
+	'music/5.mp3'
+}
+
+loadscreen 'index.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
